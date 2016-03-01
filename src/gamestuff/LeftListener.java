71a82f314c914b9607d94ff40ec8376843af8fb8
Kenneth Humphries <kennyboyeleete@gmail.com>
2016-03-01 17:37:44 -0500

    Added listener classes and changed rules of game.

---
+++ src/gamestuff/LeftListener.java
@@ -0,0 +1,72 @@
+package gamestuff;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class LeftListener implements KeyListener {
+
+	@Override
+	public void keyPressed(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void keyReleased(KeyEvent arg0) {
+		// System.out.println("Key Released.");
+		if (findValue(Game.leftKeyList, arg0.getKeyCode())) {
+			// System.out.println("Is argument correct?");
+			if (arg0.getKeyCode() == Game.leftKey) {
+				// System.out.println("Yes.");
+				Game.n -= 5;
+				int temp = Game.leftKey;
+				while (temp == Game.leftKey) {
+					int x = (int) (Math.random() * 4);
+					// System.out.println("x = " + x);
+					// System.out.println("leftKey = " + Game.leftKey);
+					// System.out.println("temp = " + temp);
+					if (x == 0) {
+						temp = 65; // A
+						Game.leftPrompt.setText("A");
+					} else if (x == 1) {
+						temp = 87; // W
+						Game.leftPrompt.setText("W");
+					} else if (x == 2) {
+						temp = 68; // D
+						Game.leftPrompt.setText("D");
+					} else if (x == 3 || x == 4) {
+						temp = 83; // S
+						Game.leftPrompt.setText("S");
+					}
+				}
+				Game.leftKey = temp;
+				Game.bar.setValue(Game.n);
+				Game.checkWin();
+			} else {
+				// System.out.println("No");
+				Game.n += 5;
+				Game.bar.setValue(Game.n);
+				Game.checkWin();
+			}
+		}
+		// System.out.println("Finished");
+	}
+
+	@Override
+	public void keyTyped(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public static boolean findValue(int[] list, int value) {
+		// System.out.println("Finding values");
+		for (int i = 0; i <= list.length - 1; i++) {
+			if (list[i] == value) {
+				// System.out.println("Value found");
+				return true;
+			}
+			// System.out.println("Value NOT found");
+		}
+		return false;
+	}
+}
---
+++ src/gamestuff/RightListener.java
@@ -0,0 +1,68 @@
+package gamestuff;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class RightListener implements KeyListener {
+
+	@Override
+	public void keyPressed(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void keyReleased(KeyEvent arg0) {
+		// System.out.println("Key Released.");
+		if (findValue(Game.rightKeyList, arg0.getKeyCode())) {
+			// System.out.println("Is argument correct?");
+			if (arg0.getKeyCode() == Game.rightKey) {
+				Game.n += 5;
+				int temp = Game.rightKey;
+				while (temp == Game.rightKey) {
+					int x = (int) (Math.random() * 4);
+					if (x == 0) {
+						temp = 37; // LEFT
+						Game.rightPrompt.setText("LEFT");
+					} else if (x == 1) {
+						temp = 38; // UP
+						Game.rightPrompt.setText("UP");
+					} else if (x == 2) {
+						temp = 39; // RIGHT
+						Game.rightPrompt.setText("RIGHT");
+					} else if (x == 3 || x == 4) {
+						temp = 40; // DOWN
+						Game.rightPrompt.setText("DOWN");
+					}
+				}
+				Game.rightKey = temp;
+				Game.bar.setValue(Game.n);
+				Game.checkWin();
+			} else {
+				// System.out.println("No");
+				Game.n -= 5;
+				Game.bar.setValue(Game.n);
+				Game.checkWin();
+			}
+		}
+		// System.out.println("Finished");
+	}
+
+	@Override
+	public void keyTyped(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public static boolean findValue(int[] list, int value) {
+		// System.out.println("Finding values");
+		for (int i = 0; i <= list.length - 1; i++) {
+			if (list[i] == value) {
+				// System.out.println("Value found");
+				return true;
+			}
+			// System.out.println("Value NOT found");
+		}
+		return false;
+	}
+}
